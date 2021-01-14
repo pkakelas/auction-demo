6742378888adebb2c8be2ab3f4692f90a026d543
Dimitris Lamprinos <pkakelas@gmail.com>
2021-01-14 15:33:05 +0200

    Add bidders

--- index.py
+++ index.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 from random import shuffle, randint
 
 from engine import Auction
-from players import MiddleBidder, AllInBidder, IncreaseBidder
+from players import MiddleBidder, AllInBidder, IncreaseBidder, SameBidder, DoubleUpBidder, DistributeBidder
 
-TRIALS = 300
+TRIALS = 100
 START_BALANCE = 10e6
 BIDDERS = [
     {
@@ -12,33 +12,46 @@ BIDDERS = [
         'name': 'Soula'
     },
     {
-        'strategy': AllInBidder,
+        'strategy': DoubleUpBidder,
         'name': 'Koula'
+    },
+    {
+        'strategy': DistributeBidder,
+        'name': 'Loulis'
     },
     {
         'strategy': IncreaseBidder,
         'name': 'Makis'
-    }
+    },
+    {
+        'strategy': SameBidder,
+        'name': 'Lakis'
+    },
+    {
+        'strategy': AllInBidder,
+        'name': 'Toula'
+    },
 ]
 
 players = {}
 for bidder in BIDDERS:
-    players[bidder['name']] = bidder['strategy'](bidder['name'], START_BALANCE)
+    players[bidder['name']] = bidder['strategy'](bidder['name'], START_BALANCE, TRIALS)
 
 print(players)
 
-def get_random_baseline_amount():
-    return randint(50, 10000)
+def get_random_baseline_amount(prev_baseline_amount):
+    return prev_baseline_amount + randint(1, 10000)
 
 def main():
     winners = {}
+    baseline_amount = 0
 
     for _ in range(TRIALS):
-        baseline_amount = get_random_baseline_amount()
+        baseline_amount = get_random_baseline_amount(baseline_amount)
 
         auction = Auction(baseline_amount, players.copy())
         winner = auction.run()
-
+        [player.announce_winner(winner) for player in players.values()]
         winners[winner] = winners[winner] + 1 if winner in winners else 1
 
     plt.bar(list(winners.keys()), list(winners.values()))
--- players.py
+++ players.py
@@ -1,55 +1,72 @@
 # Just increased amount by 10 each time
 
 class AbstractBidder:
-    def __init__(self, name, balance):
+    def __init__(self, name, balance, trials):
         self.name = name
         self.balance = balance
+        self.trials = trials
+        self.won_trials = 0
+        self.won_prev_trial = False
+        self.current_trial = 0
+        self.prev_bid = 0
 
     def pay(self, amount):
         self.balance -= amount
         return self.balance > 0
+
+    def announce_winner(self, winner):
+        self.current_trial += 1
+
+        if winner == self.name:
+            self.won_prev_trial = True
+            self.won_trials += 1
+        else:
+            self.won_prev_trial = False
 
     def get_balance(self):
         return self.balance
 
     def bid(self, baseline_amount):
         bid = self.make_bid(baseline_amount)
-        if bid > self.balance:
-            raise Exception("Bidder bidding money he cannot afford")
+        self.prev_bid = bid
 
-        return bid
+        # Check if cannot afford the bid
+        return bid if bid <= self.balance else 0
 
     # Main strategy implementation
     def make_bid(self, max_bid_amount):
         raise Exception("Should be implemented")
 
-# If player has money then increase max_bid by 10 else quit
-class RationalBidder(AbstractBidder):
-    def make_bid(self, max_bid_amount):
-        to_bet = max_bid_amount + (.1 * max_bid_amount)
-        if to_bet <= self.balance:
-            return to_bet
+class DoubleUpBidder(AbstractBidder):
+    def make_bid(self, baseline_amount):
+        if self.prev_bid == 0:
+            bid = baseline_amount
 
-        return 0
-
-# Give all of his money to the first product
-class RiskyBidder(AbstractBidder):
-    # Main strategy implementation
-    def make_bid(self, max_bid_amount):
-        return self.balance
+        bid = self.prev_bid if self.won_prev_trial else self.prev_bid * 2
+        return bid
 
 class IncreaseBidder(AbstractBidder):
     def make_bid(self, baseline_amount):
-        to_bet = baseline_amount + (.1 * baseline_amount)
-
-        return to_bet if to_bet <= self.balance else 0
+        return baseline_amount + (.1 * baseline_amount)
 
 class MiddleBidder(AbstractBidder):
     def make_bid(self, baseline_amount):
-        to_bet = baseline_amount + (self.balance / 2)
-
-        return to_bet if to_bet <= self.balance else 0
+        return baseline_amount + (self.balance / 2)
 
 class AllInBidder(AbstractBidder):
     def make_bid(self, max_bid_amount):
         return self.balance
+
+class SameBidder(AbstractBidder):
+    def make_bid(self, baseline_amount):
+        return 5000
+
+class DistributeBidder(AbstractBidder):
+    def make_bid(self, baseline_amount):
+        if self.prev_bid == 0:
+            self.prev_bid = baseline_amount
+
+        trials_left = self.trials - self.current_trial
+        bid = self.prev_bid if self.won_prev_trial else int(self.balance / trials_left)
+
+        return bid
